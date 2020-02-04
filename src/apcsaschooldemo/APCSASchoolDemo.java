67b46d0131e7135965c0cd8644facebc7ad90452
mike <mike@DESKTOP-H98JHIB.san.rr.com>
2020-02-03 18:39:35 -0800

    test

--- src/apcsaschooldemo/APCSASchoolDemo.java
+++ src/apcsaschooldemo/APCSASchoolDemo.java
@@ -16,6 +16,7 @@ public class APCSASchoolDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    	System.out.println("Hello Git Hub");
     }
     
 }
